f9e8008e09f23635698502e7603e9f382c8b815f
lipeng <lipeng@shein.com>
2019-12-04 11:11:42 +0800

    提交pom文件和修改注释

--- src/test/java/com/primary/example/andalgorithm/BitAlgorithm.java
+++ src/test/java/com/primary/example/andalgorithm/BitAlgorithm.java
@@ -16,13 +16,13 @@ public class BitAlgorithm {
         int num2 = 9;
         System.out.println("8的二进制为：" + Integer.toBinaryString(num1));
         System.out.println("9的二进制为：" + Integer.toBinaryString(num2));
-        // 与运算二进制结果：1000
+        // 与运算二进制结果：1000，都为1结果为1，否则为0
         System.out.println(num1 & num2);
-        // 或运算二进制结果：1001
+        // 或运算二进制结果：1001，其中一个为1，结果为1，否则为0
         System.out.println(num1 | num2);
-        // 非运算二进制结果：0111
+        // 非运算二进制结果：0111，取反
         System.out.println(~num1);
-        // 异或二进制结果：0001
+        // 异或二进制结果：0001，相同取0，不同取1ª
         System.out.println(num1 ^ num2);
     }
 }
