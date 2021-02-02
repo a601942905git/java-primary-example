8f39bef20dfc209ca06d509f666cb82b7dcfef37
lipeng <lipeng@shein.com>
2021-02-02 17:35:41 +0800

    修改自定义锁逻辑

--- src/test/java/com/primary/example/thread/lock/Mutex.java
+++ src/test/java/com/primary/example/thread/lock/Mutex.java
@@ -1,5 +1,7 @@
 package com.primary.example.thread.lock;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.Serializable;
@@ -11,6 +13,19 @@ import java.util.concurrent.locks.Lock;
 /**
  * com.primary.example.thread.lock.Mutex
  *
+ * 锁：
+ * 1.限制对共享资源的访问
+ * 2.可重入性
+ * 3.互斥性
+ *
+ * 公平与非公平：
+ * 公平：每次从队列取第一个节点执行锁竞争
+ * 非公平：当前线程执行锁竞争
+ *
+ * 共享与独占：
+ * 共享：同一时刻允许多个线程获取锁，如读与读之间
+ * 独占：同一时刻只允许一个线程获取锁
+ *
  * 自定义锁实现步骤：
  * 1、定义一个静态内部类Sync
  * 2、静态内部类Sync继承AbstractQueuedSynchronizer
@@ -21,6 +36,7 @@ import java.util.concurrent.locks.Lock;
  * @author lipeng
  * @date 2019/11/15 上午10:09
  */
+@Slf4j
 public class Mutex implements Lock, Serializable {
 
     public static class Sync extends AbstractQueuedSynchronizer {
@@ -28,27 +44,45 @@ public class Mutex implements Lock, Serializable {
         @Override
         protected boolean tryAcquire(int acquires) {
             assert acquires == 1;
-            if (compareAndSetState(0, 1)) {
-                setExclusiveOwnerThread(Thread.currentThread());
-                return true;
+            Thread currentThread = Thread.currentThread();
+            int state = getState();
+            // 可以获取锁
+            if (state == 0) {
+                // 成功获取锁
+                if (compareAndSetState(0, acquires)) {
+                    setExclusiveOwnerThread(currentThread);
+                    return true;
+                }
+            } else {
+                // 如果当前线程已经持有锁，则需要支持重入功能
+                if (getExclusiveOwnerThread() == currentThread) {
+                    setState(state + acquires);
+                    return true;
+                }
             }
             return false;
         }
 
         @Override
-        protected boolean tryRelease(int acquires) {
-            assert acquires == 1;
-            if (getState() == 0) {
+        protected boolean tryRelease(int release) {
+            if (Thread.currentThread() != getExclusiveOwnerThread()) {
                 throw new IllegalMonitorStateException();
             }
-            setState(0);
-            setExclusiveOwnerThread(null);
-            return true;
+
+            boolean free = false;
+            int c = getState() - release;
+            if (c == 0) {
+                free = true;
+                setExclusiveOwnerThread(null);
+            }
+
+            setState(c);
+            return free;
         }
 
         @Override
         protected boolean isHeldExclusively() {
-            return getState() == 1;
+            return getExclusiveOwnerThread() == Thread.currentThread();
         }
 
         Condition newCondition() {
@@ -99,6 +133,6 @@ public class Mutex implements Lock, Serializable {
     }
 
     public boolean isHeldExclusively() {
-        return isHeldExclusively();
+        return sync.isHeldExclusively();
     }
 }
--- src/test/java/com/primary/example/thread/lock/MutexTest.java
+++ src/test/java/com/primary/example/thread/lock/MutexTest.java
@@ -1,6 +1,12 @@
 package com.primary.example.thread.lock;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * com.primary.example.thread.lock.MutexTest
@@ -8,29 +14,57 @@ import org.junit.Test;
  * @author lipeng
  * @date 2019/11/15 上午10:27
  */
+@Slf4j
 public class MutexTest {
 
     private final Mutex mutex = new Mutex();
+
+    private final CountDownLatch countDownLatch = new CountDownLatch(1);
 
     private static final Integer THREAD_NUMBER = 3;
 
     @Test
     public void testMyLock() throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(THREAD_NUMBER);
         for (int i = 0; i < THREAD_NUMBER; i++) {
-            new Thread(() -> {
-                try {
-                    mutex.lock();
-                    Thread.sleep(300000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } finally {
-                    mutex.unlock();
-                }
-            }).start();
+            executorService.submit(new Task(countDownLatch));
         }
 
-        while (true) {
+        countDownLatch.countDown();
 
+        TimeUnit.SECONDS.sleep(10);
+    }
+
+    class Task implements Runnable {
+        private final CountDownLatch countDownLatch;
+
+        public Task(CountDownLatch countDownLatch) {
+            this.countDownLatch = countDownLatch;
+        }
+
+        @Override
+        public void run() {
+            try {
+                countDownLatch.await();
+            } catch (InterruptedException e) {
+                log.error("thread interrupted exception：", e);
+            }
+
+            try {
+                mutex.lock();
+                log.info("current thread：{} acquire lock1，execute task", Thread.currentThread().getName());
+                mutex.lock();
+                log.info("current thread：{} acquire lock2，execute task", Thread.currentThread().getName());
+                TimeUnit.SECONDS.sleep(1);
+            } catch (Exception e) {
+                log.info("execute exception：", e);
+            } finally {
+                mutex.unlock();
+                log.info("current thread：{} release lock1", Thread.currentThread().getName());
+                mutex.unlock();
+                log.info("current thread：{} release lock2", Thread.currentThread().getName());
+            }
         }
     }
+
 }
