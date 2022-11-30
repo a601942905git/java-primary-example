3f6718c3d8809abdc60295a798452d54e5985a6b
10007616 <lipeng@shein.com>
2022-11-30 17:56:33 +0800

    ip工具类添加注释

--- src/test/java/com/primary/example/util/IpUtils.java
+++ src/test/java/com/primary/example/util/IpUtils.java
@@ -8,7 +8,7 @@ import java.util.Enumeration;
 
 /**
  * com.primary.example.util.IpUtils
- *
+ * 127.0.0.1，通常被称为本地回环地址
  * @author lipeng
  * @date 2022/2/17 7:43 PM
  */
@@ -16,9 +16,11 @@ public class IpUtils {
 
     public static String getHostAddress() {
         try {
+            // 遍历所有网口
             Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
             while (networkInterfaces.hasMoreElements()) {
                 NetworkInterface networkInterface = networkInterfaces.nextElement();
+                // 遍历网口对应地址
                 Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
                 while (inetAddresses.hasMoreElements()) {
                     InetAddress inetAddress = inetAddresses.nextElement();
